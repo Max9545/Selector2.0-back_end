72965d31ceaa9b5f5c7a8852b568d2b4c6a9dcac
Jordan Beck <jordanfbeck@gmail.com>
2021-05-13 19:06:52 -0600

    Adds intial test and code for discogs service
    Index action in discogs controller is returning album info now
    
    Co-authored-by: Megan Gonzales <62969459+MGonzales26@users.noreply.github.com>
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>

---
+++ app/services/discogs_service.rb
@@ -0,0 +1,31 @@
+class DiscogsService
+  def self.conn
+    # require "pry"; binding.pry
+    faraday = Faraday.new(url: 'https://api.discogs.com')
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_album(album)
+    response = conn.get('/database/search') do |f|
+      f.params['key'] = ENV['key']
+      f.params['secret'] = ENV['secret']
+      f.params['q'] = album
+      f.params['format'] = "album"
+    end
+    album_q = parse(response)[:results][0][:master_id]
+    album_q
+  end
+
+  def self.get_album_resource(album)
+    id = get_album(album).to_s
+    response = Faraday.get("https://api.discogs.com/masters/#{id}")
+    parse(response)
+  end
+
+  def self.get_album_data(album)
+    self.get_album_resource(album)
+  end
+end
