1b22e723a752e5e0266378eff5cecd81659a0425
Jordan Beck <jordanfbeck@gmail.com>
2021-05-17 14:48:33 -0600

    Make a cassette
    Co-authored-by: Megan Gonzales <62969459+MGonzales26@users.noreply.github.com>
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>

--- spec/requests/api/v1/discogs_request_spec.rb
+++ spec/requests/api/v1/discogs_request_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Discogs API request', type: :request do
   it 'Returns realease data for album query params from user', :vcr do
 
+
   # get api_v1_discogs_path, params: {album: "The Payback"}
 
   # forecast = JSON.parse(response.body, symbolize_names:true)
