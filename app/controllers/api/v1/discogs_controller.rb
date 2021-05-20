455a69fc83ed4c451244192bb1cc66ac1beed468
Jordan Beck <jordanfbeck@gmail.com>
2021-05-20 17:03:47 -0600

    Adds random album, and random ten albums methods

--- app/controllers/api/v1/discogs_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::DiscogsController < ApplicationController
-  def index
-    # @album = DiscogsService.get_album(params[:album])
-    @album = DiscogsService.get_album_data(params[:album])
-    # require "pry"; binding.pry
-  end
-end
