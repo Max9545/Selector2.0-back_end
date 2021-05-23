64486bb630dc7668b957626a4be9d69721fb494b
Adam Bowers <adam.j.bowers@gmail.com>
2021-05-23 11:58:26 -0600

    Add service tests

--- spec/services/discogs_services_spec.rb
+++ spec/services/discogs_services_spec.rb
@@ -11,8 +11,6 @@ describe 'Discogs Service API' do
     it '::get_album' do
       album = DiscogsService.get_album('War & Leisure')
 
-      require "pry"; binding.pry
-
       expect(album).to be_a(Hash)
       expect(album.keys).to match_array([:pagination, :results])
       expect(album[:pagination].keys).to match_array([:page, :pages, :per_page, :items, :urls])
@@ -36,19 +34,129 @@ describe 'Discogs Service API' do
         expect(l).to be_a(String)
       end
 
+      expect(album[:results][0][:type]).to be_a(String)
+      expect(album[:results][0][:genre]).to be_a(Array)
 
+      album[:results][0][:genre].each do |t|
+        expect(t).to be_a(String)
+      end
 
-      # expect(album[:results][0][:label]).to be_an(Array)
-      # expect(album[:results][0][:country]).to be_a(String)
-      # expect(album[:results][0][:country]).to be_a(String)
+      album[:results][0][:style].each do |s|
+        expect(s).to be_a(String)
+      end
+
+      expect(album[:results][0][:id]).to be_an(Integer)
+
+      album[:results][0][:barcode].each do |b|
+        expect(b).to be_a(String)
+      end
+
+      expect(album[:results][0][:master_id]).to be_an(Integer)
+      expect(album[:results][0][:master_url]).to be_a(String)
+      expect(album[:results][0][:uri]).to be_a(String)
+      expect(album[:results][0][:catno]).to be_a(String)
+      expect(album[:results][0][:title]).to be_a(String)
+      expect(album[:results][0][:thumb]).to be_a(String)
+      expect(album[:results][0][:cover_image]).to be_a(String)
+      expect(album[:results][0][:resource_url]).to be_a(String)
+      expect(album[:results][0][:community]).to be_a(Hash)
+      expect(album[:results][0][:community].keys).to match_array([:want, :have])
+      expect(album[:results][0][:community][:want]).to be_an(Integer)
+      expect(album[:results][0][:community][:have]).to be_an(Integer)
     end
 
-    xit '::get_album_resource' do
-      album = DiscogsService.get_album_resource('War & Lesire')
+    it '::get_album_resource' do
+      album = DiscogsService.get_album_resource('War & Leisure')
 
-      # require "pry"; binding.pry
+      expect(album).to be_a(Hash)
+      expect(album.keys).to match_array([:id, :main_release, :most_recent_release, :resource_url, :uri, :versions_url, :main_release_url, :most_recent_release_url, :num_for_sale, :lowest_price, :images, :genres, :styles, :year, :tracklist, :artists, :title, :data_quality, :videos, :cover_image])
+      expect(album[:id]).to be_an(Integer)
+      expect(album[:main_release]).to be_an(Integer)
+      expect(album[:most_recent_release]).to be_an(Integer)
+      expect(album[:resource_url]).to be_a(String)
+      expect(album[:uri]).to be_a(String)
+      expect(album[:versions_url]).to be_a(String)
+      expect(album[:main_release_url]).to be_a(String)
+      expect(album[:most_recent_release_url]).to be_a(String)
+      expect(album[:num_for_sale]).to be_an(Integer)
+      expect(album[:lowest_price]).to be_an(Float)
+      expect(album[:images][0]).to be_a(Hash)
+      expect(album[:images][0].keys).to match_array([:type, :uri, :resource_url, :uri150, :width, :height])
+      expect(album[:images][0][:type]).to be_a(String)
+      expect(album[:images][0][:uri]).to be_a(String)
+      expect(album[:images][0][:resource_url]).to be_a(String)
+      expect(album[:images][0][:uri150]).to be_a(String)
+      expect(album[:images][0][:width]).to be_an(Integer)
+      expect(album[:images][0][:height]).to be_an(Integer)
+      expect(album[:genres]).to be_an(Array)
 
-      expect(album).to eq(2)
+      album[:genres].each do |g|
+        expect(g).to be_a(String)
+      end
+
+      expect(album[:styles]).to be_an(Array)
+
+      album[:styles].each do |s|
+        expect(s).to be_a(String)
+      end
+
+      expect(album[:year]).to be_an(Integer)
+      expect(album[:tracklist]).to be_an(Array)
+
+      album[:tracklist].each do |t|
+        expect(t).to be_a(Hash)
+        expect(t[:position]).to be_a(String)
+        expect(t[:type_]).to be_a(String)
+        expect(t[:title]).to be_a(String)
+        expect(t[:extraartists]).to be_an(Array)
+      end
+
+      expect(album[:tracklist][0][:extraartists]).to be_an(Array)
+
+      album[:tracklist][0][:extraartists].each do |e|
+        expect(e).to be_a(Hash)
+        expect(e[:name]).to be_a(String)
+        expect(e[:anv]).to be_a(String)
+        expect(e[:join]).to be_a(String)
+        expect(e[:role]).to be_an(String)
+        expect(e[:tracks]).to be_an(String)
+        expect(e[:id]).to be_an(Integer)
+        expect(e[:resource_url]).to be_an(String)
+      end
+
+      expect(album[:artists]).to be_an(Array)
+
+      album[:artists].each do |a|
+        expect(a).to be_a(Hash)
+        expect(a[:name]).to be_a(String)
+        expect(a[:anv]).to be_a(String)
+        expect(a[:join]).to be_a(String)
+        expect(a[:role]).to be_an(String)
+        expect(a[:tracks]).to be_an(String)
+        expect(a[:id]).to be_an(Integer)
+        expect(a[:resource_url]).to be_an(String)
+      end
+
+      expect(album[:title]).to be_an(String)
+      expect(album[:data_quality]).to be_an(String)
+
+      album[:videos].each do |v|
+        expect(v).to be_a(Hash)
+        expect(v[:uri]).to be_a(String)
+        expect(v[:title]).to be_a(String)
+        expect(v[:description]).to be_a(String)
+        expect(v[:duration]).to be_an(Integer)
+        expect(v[:embed]).to be_a(TrueClass)
+      end
+
+      expect(album[:cover_image]).to be_a(String)
+    end
+
+    it '::get_album_data' do
+      album = DiscogsService.get_album_data('')
+
+      expect(album[:title]).to eq('The Battle Of Los Angeles')
+      expect(album[:artists][0][:name]).to eq('Rage Against The Machine')
     end
   end
 end
